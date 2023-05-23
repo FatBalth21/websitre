2aebc1c24caba1042b4c7bbfc5d934aebd23d6dc
Ishmail Khan <moils.khan345@gmail.com>
2023-05-23 19:05:50 +0200

    added routers and controllers

--- routes.js
+++ routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const profileController = require('./Controller.js');
+const controller = require('./controller.js');
 
-router.get('/', profileController.getProfile);
+router.get('/', controller.getBasic);
+router.get('/Signup', controller.getSignup);
+router.get('/Login', controller.getLogin);
+router.get('/dashboard', controller.getDashboard);
+router.get('/Profile', controller.getProfile);
+router.post('/Userinfo', controller.postUserinfo);
+router.post('/signup', controller.postsignup);
+router.post('/login', controller.postlogin)
 
 module.exports = router;
