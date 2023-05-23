90037d34f83f7a8746177ec032cac306d9212f18
Ishmail Khan <moils.khan345@gmail.com>
2023-05-23 16:24:05 +0200

    Added comments and styles to profile

--- index.js
+++ index.js
@@ -5,174 +5,222 @@ const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const fileUpload = require('express-fileupload');
 const loggedoutheader = fs.readFileSync('./loggedoutheader.html', 'utf8');
 const loggedinheader = fs.readFileSync('./loggedinheader.html', 'utf8');
 const footer = fs.readFileSync('./footer.html', 'utf8');
 
 let storedPassword = "";
-let emailuser = "";
 let str = ' '
-let base = loggedoutheader + footer
 
+app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(session({
-  secret: 'Nanoo ghar',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false,  // set to true if using HTTPS
-  maxAge: 24*60*60*1000 // 24 hours in milliseconds
-  }
+    secret: 'Nanoo ghar',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,  // set to true if using HTTPS
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours in milliseconds
+    }
 }));
 
-app.get('/', (req, res) => { // specify endpoint(name)
-  res.send(base)
+app.get('/', (req, res) => { // specify endpoint(name) This shows the dafault page
+    customrender('/Basic.html')
+    res.send(output);
 })
-app.get('/Signup.html', function (req, res) {
-  customrender('/Signup.html')
-  res.send(output)
+app.get('/Signup.html', function (req, res) { // This shows the signup page
+    customrender('/Signup.html') // the custom render function is meant to render the header and footer for the page
+    res.send(output) // sends the output to the mainpage
 });
-app.get('/Login.html', function (req, res) {
-  customrender('/Login.html')
-  res.send(output)
-});
-app.get('/dashboard.html', (req, res) => {
-  // Check if user is authenticated
-  if (req.session.email) {
-    // If authenticated, render the dashboard page
-    customrender1('/dashboard.html')
+app.get('/Login.html', function (req, res) { // This shows the login page
+    customrender('/Login.html')
     res.send(output)
-  } else {
-    // If not authenticated, redirect to the login page
-    res.redirect('/login')
-  }
+});
+app.get('/Test.html', function (req, res) { // this is a page to test features
+    customrender1('/Test.html'); 
+    res.send(output);
+});
+
+app.get('/dashboard.html', (req, res) => {
+    // Check if user is authenticated
+    if (req.session.email) {
+        // If authenticated, render the dashboard page
+        customrender1('/dashboard.html')
+        res.send(output)
+    } else {
+        // If not authenticated, redirect to the login page
+        res.redirect('/login')
+    }
 })
 app.get('/Profile', (req, res) => {
-  const emailuser = req.session.email;
-  console.log(emailuser);
-  let username = '';
-  const credentials = fs.readFileSync('./credentials.txt', 'utf8');
-  const lines = credentials.split('\n');
-  lines.forEach((line, index) => {
-    if (line.includes(emailuser)) {
-      const lineArray = line.split(',');
-      username = lineArray[0] + lineArray[1];
-    }
-  });
-  const userFile = './userinfo/user_address_num/' + emailuser + '.txt';
-  const infoUploaded = fs.existsSync(userFile);
-  let output = '';
-  if (infoUploaded) {
-    // If both files exist, read the address and phone number and render the profile page
-    const userFileContents = fs.readFileSync(userFile, 'utf8');
-    const [address, phone] = userFileContents.split('\n');
-    output = customrenderprofile('/dashboard.html');
-    output += `
+    const emailuser = req.session.email;// requests the email of the session using bodyparser middleware
+    console.log(emailuser);
+    let username = '';
+    const credentials = fs.readFileSync('./credentials.txt', 'utf8'); // reads the file with the user's details
+    const lines = credentials.split('\n'); //splits said files to multiple lines ofr indexing
+    lines.forEach((line, index) => { // code for finding the emailuser and the line for which the firstname and lastname exist to create a username
+        if (line.includes(emailuser)) {
+            const lineArray = line.split(',');
+            username = lineArray[0] + ' ' + lineArray[1];
+        }
+    });
+    const userFile = './userinfo/user_address_num/' + emailuser + '.txt';//file that stores the users address and phone number
+    const infoUploaded = fs.existsSync(userFile); // checks for existence of said user
+    const picPath = "./userinfo/userpfp/" + emailuser + '.png';// checks for picture
+    let output = '';
+    if (infoUploaded) {
+        // If both files exist, read the address and phone number and render the profile page
+        const userFileContents = fs.readFileSync(userFile, 'utf8');
+        const [address, phone] = userFileContents.split('\n');
+        const pic = fs.readFileSync(picPath).toString('base64');//converts file to base64 to be displayed on the website
+        output = customrenderprofile('/abcd.html');
+        output += ` 
+        <div class="profile-info">
       <h1>Welcome ${username}</h1>
       <p>Address: ${address}</p>
       <p>Phone Number: ${phone}</p>
+      <p>${emailuser}</p>
+      <img src="data:image/png;base64,${pic}"  alt="User Profile Picture size" width="100px" height="100px">
+      </div>
+
     `;
-    
-  } else {
-    // If either file does not exist, render the profile page with a message
-    output = customrenderprofile('/Profile.html');
-    output += `
+
+    } else {
+        // If either file does not exist, render the profile page with a message
+        output = customrenderprofile('/Profile.html');
+        output += `
       <h1>Welcome ${username}</h1>
       <p>You have not uploaded your address and phone number yet.</p>
     `;
-  }
-  res.send(output);
+    }
+    output = customrenderfooter(output);
+    res.send(output);
 });
 
 
 app.post('/dashboard', (req, res) => {
 
 })
-app.post('/userinfo', (req,res) => {
-  const emailuser = req.session.email;
-  const userFile = './userinfo/user_address_num/' + emailuser + '.txt';
-  const infoUploaded = fs.existsSync(userFile);
-  // Check if user has already uploaded information
-  if (infoUploaded) {
-    // If both files already exist, redirect to the profile page
-    res.redirect('/Profile');
-  } else {
-    const address = req.body.address;
-    const number = req.body.phone;
-    // const pic = req.body.picture; // Not sure what this is for
 
-    // Otherwise, write the address and number to the files and redirect to the profile page
-    fs.writeFileSync(userFile, address + '\n' + number, 'utf8');
-    //fs.writeFileSync('./userinfo/userpfp/' + emailuser + '.png', pic, 'binary')
+app.post('/upload', (req, res) => {//test page for images
 
-    res.redirect('/Profile');
-  }
 });
 
-app.post('/signup', (req, res) => {
-  const fname = req.body.firstname;
-  const lname = req.body.lastname;
-  const email = req.body.email;
-  const password = req.body.password;
-  fs.readFile('./credentials.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    const lines = data.split('\n'); // split the data into an array of lines
-    let emailFound = false;
-    lines.forEach((line, index) => {
-      if (line.includes(email)) {
-        emailFound = true;
-        res.send('Choose another email');
-      }
+
+app.post('/userinfo', (req, res) => {
+    const emailuser = req.session.email;
+    const userFile = './userinfo/user_address_num/' + emailuser + '.txt';
+    const infoUploaded = fs.existsSync(userFile);
+    // Check if user has already uploaded information
+    if (infoUploaded) {
+        // If both files already exist, redirect to the profile page
+        res.redirect('/Profile');
+    } else {
+        const address = req.body.address;
+        const number = req.body.phone;
+
+                // Write the address and number to the files and redirect to the profile page
+                fs.writeFileSync(userFile, address + '\n' + number, 'utf8');
+                if (!req.files || !req.files.file) {
+                    return res.status(400).send('No file uploaded.');
+                  }
+                
+                  const file = req.files.file;
+                  const fileExtension = path.extname(file.name);
+                  const uniqueFilename = req.session.email + fileExtension;
+                  const filePath = './userinfo/userpfp/' + uniqueFilename;
+
+                  // Save the file to disk
+                  file.mv(filePath, (error) => {
+                    if (error) {
+                      console.error(error);
+                      return res.status(500).send('Failed to save the file.');
+                    }
+                
+
+                  });
+                res.redirect('/Profile');
+        };
     });
-    if (!emailFound) {
-      fs.appendFile('credentials.txt', `${fname},${lname},${email},${password}\n`, (err) => {
+
+
+app.post('/signup', (req, res) => {
+   // requests data from farm and stores in a text file called credetnails.txt
+    const fname = req.body.firstname;
+    const lname = req.body.lastname;
+    const email = req.body.email;
+    const password = req.body.password;
+    fs.readFile('./credentials.txt', 'utf8', (err, data) => {
         if (err) throw err;
-        console.log('You signed up');
-        res.redirect('./Login.html');
-      });
-    }
-  });
+        const lines = data.split('\n'); // split the data into an array of lines
+        let emailFound = false;
+        lines.forEach((line, index) => {
+            if (line.includes(email)) {
+                emailFound = true;
+                res.send('Choose another email');
+            }
+        });
+        if (!emailFound) {
+            const writeStream = fs.createWriteStream('credentials.txt', { flags: 'a' });
+            writeStream.write(`${fname},${lname},${email},${password}\n`, (err) => {
+                if (err) throw err;
+                console.log('You signed up');
+                res.redirect('/login.html');
+            });
+        }
+    });
 });
 
 app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  console.log('You logged in');
-  fs.readFile('./credentials.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    const lines = data.split('\n'); // split the data into an array of lines
-    let storedPassword = '';
-    lines.forEach((line, index) => {
-      if (line.includes(email)) {
-        const lineArray = line.split(',');
-        storedPassword = lineArray[3];
-      }
+    const email = req.body.email;
+    const password = req.body.password;
+    fs.readFile('./credentials.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        const lines = data.split('\n'); // split the data into an array of lines
+        lines.forEach((line, index) => {
+            if (line.includes(email)) {
+                const lineArray = line.split(',');
+                storedPassword = lineArray[3];
+            }
+        });
+        if (storedPassword === password) {
+            req.session.email = email;
+            emailuser = email;
+            res.redirect('/dashboard.html')
+            console.log('You logged in');
+
+        } else {
+            res.send('Invalid credentials');
+        }
     });
-    if (storedPassword === password) {
-      req.session.email = email;
-      emailuser = email;
-      res.redirect('/dashboard.html')
-    } else {
-      res.send('Invalid credentials');
-    }
-  });
 });
+//allows express to use images from designated libraries
+app.use(express.static('./userinfo/userpfp/'));
+app.use(express.static('./images'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 function customrender(input) {
-  str = fs.readFileSync(__dirname + input, 'utf8')
-  output = loggedoutheader + str + footer
-  return (output)
+    str = fs.readFileSync(__dirname + input, 'utf8')
+    output = loggedoutheader + str + footer
+    return (output)
 }
 function customrender1(input) {
-  str = fs.readFileSync(__dirname + input, 'utf8')
-  output = loggedinheader + str + footer
-  return (output)
+    str = fs.readFileSync(__dirname + input, 'utf8')
+    output = loggedinheader + str + footer
+    return (output)
 }
 function customrenderprofile(input) {
-  str = fs.readFileSync(__dirname + input, 'utf8')
-  output = loggedinheader + str + footer
-  return (output)
+    str = fs.readFileSync(__dirname + input, 'utf8')
+    output = loggedinheader + str ;
+    return (output)
+}
+function customrenderfooter(input) {
+    str = input;
+    output = str + footer ;
+    return (output)
 }
